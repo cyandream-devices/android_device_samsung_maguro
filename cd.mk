01eb5f439a6f8c1fed2582a243575d97858b75c9
yanniks <kontakt@yanniks.de>
2013-09-15 13:39:59 +0200

    changed prefix from cm to cd

--- cd.mk
+++ cd.mk
@@ -8,7 +8,7 @@ PRODUCT_RELEASE_NAME := GN-GSM
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
+# Inherit some common CD stuff.
 $(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Enhanced NFC
@@ -19,7 +19,7 @@ $(call inherit-product, device/samsung/maguro/full_maguro.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
-PRODUCT_NAME := cm_maguro
+PRODUCT_NAME := cd_maguro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
